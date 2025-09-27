cea877e15a1a8115db0155c5e663cb694349fbd6
dhimankringle <107526337+dhimankringle@users.noreply.github.com>
2025-09-27 21:55:44 +0530

    code fix

--- app/models/devices.py
+++ app/models/devices.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import relationship, mapped_column
 from sqlalchemy import Column, String, BigInteger, ForeignKey, Enum, TIMESTAMP, DateTime, Boolean, Integer, func, Index, BIGINT, Text, event, DECIMAL, JSON, Time, CheckConstraint, Date, TEXT
 from sqlalchemy.orm import Mapped
 from app.config.db_connection import Base
+from typing import Optional
 from typing import Any
 
 class Device(Base):
@@ -9,10 +10,10 @@ class Device(Base):
 
     id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True)
     user_id: Mapped[int] = mapped_column(BigInteger, ForeignKey('deelflow_user.id', ondelete='CASCADE'), nullable=False)
-    device_name: Mapped[str | None] = mapped_column(String(100), nullable=True)
-    device_ip: Mapped[str | None] = mapped_column(String(45), nullable=True)
-    device_os: Mapped[str | None] = mapped_column(String(80), nullable=True)
-    location_json: Mapped[Any | None] = mapped_column(JSON, nullable=True)
+    device_name: Mapped[Optional[str]] = mapped_column(String(100), nullable=True)
+    device_ip: Mapped[Optional[str]] = mapped_column(String(45), nullable=True)
+    device_os: Mapped[Optional[str]] = mapped_column(String(80), nullable=True)
+    location_json: Mapped[Optional[Any]] = mapped_column(JSON, nullable=True)
     created_at: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP, server_default=func.now(), nullable=False)
     last_login_at: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP, server_default=func.now(), onupdate=func.now(), nullable=False)
 
